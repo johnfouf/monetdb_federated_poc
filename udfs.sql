f3ce7708b0b0c6d51cbdd91e19a628e8ebb805c6
openaire <openaire@dl126.madgik.di.uoa.gr>
2020-07-10 13:49:51 +0300

    asynchronous concurrent version - add sleep 4 seconds to local udf to check concurrency

--- udfs.sql
+++ udfs.sql
@@ -8,43 +8,8 @@ LANGUAGE PYTHON {
 CREATE or replace AGGREGATE numpy_count(val INTEGER) 
 RETURNS INTEGER 
 LANGUAGE PYTHON {
+    import time
+    time.sleep(4)
     return val.size
 };
 
-
-select * from python_aggregate(c2) from data2 group by c1;
-
-
-CREATE AGGREGATE python_aggregate(val INTEGER) 
-RETURNS INTEGER 
-LANGUAGE PYTHON {
-    try:
-        unique = numpy.unique(aggr_group)
-        x = numpy.zeros(shape=(unique.size))
-        for i in range(0, unique.size):
-            x[i] = numpy.sum(val[aggr_group==unique[i]])
-    except NameError:
-        # aggr_group doesn't exist. no groups, aggregate on all data
-        x = numpy.sum(val)
-    return(x)
-};
-
-
-
-
-CREATE or replace AGGREGATE python_aggregate2(val INTEGER) 
-RETURNS INTEGER 
-LANGUAGE PYTHON {
-    from cffi import FFI
-    ffi = FFI()
-    arr_vals = ffi.from_buffer('int[]', val)
-    arr_groups = ffi.from_buffer('int[]', aggr_group)
-    
-    res = [0]*numpy.unique(aggr_group).size
-    j = 0
-    for v in arr_vals:
-        res[arr_groups[j]] +=  v
-        j+=1
-    
-    return numpy.array(res) 
-};
