9290413f890e2e91bdbea29e497234b9224f6b75
johnfouf <johnfouf@gmail.com>
2020-08-31 12:52:34 +0300

    Update pearson.py
    Alternative sql query

--- algorithms/pearson.py
+++ algorithms/pearson.py
@@ -3,3 +3,5 @@ def _local(viewlocaltable, parameters, attr):
     
 def _global(globaltable, parameters, attr):
     return "select * from pearson_global((select * from %s));" %globaltable
+
+## select pearson_global(sum(sx),sum(sxx),sum(sxy),sum(sy),sum(syy),sum(n)) from globaltable;
